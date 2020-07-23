af34e00ddc052dfe3aa186ab2156f2a263262823
LeoVS09 <leovs09@gmail.com>
2020-07-24 01:14:27 +0300

    change model format

--- src/save_and_restore.py
+++ src/save_and_restore.py
@@ -1,24 +1,13 @@
 import tensorflow as tf
 
-PATH_TO_SAVED_MODEL='saved_models/main'
-PATH_TO_SAVE_HD5_MODEL='saved_models/main.h5'
+PATH_TO_SAVED_MODEL='saved_models/main.h5'
 
-# Save model in SavedModel format, which can be fully respored
+# Save model in H5 format, which can be fully respored
 def save(model):
     model.save(PATH_TO_SAVED_MODEL)
 
-def saveHD5(model):
-    model.save(PATH_TO_SAVE_HD5_MODEL)
-
 def load():
     new_model = tf.keras.models.load_model(PATH_TO_SAVED_MODEL)
-
-    # Check its architecture
-    new_model.summary()
-    return new_model
-
-def loadHD5():
-    new_model = tf.keras.models.load_model(PATH_TO_SAVE_HD5_MODEL)
 
     # Check its architecture
     new_model.summary()

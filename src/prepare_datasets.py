1bdbf9075ff4db77d514963dd489f0655d4d0bde
LeoVS09 <leovs010@gmail.com>
2021-03-01 02:55:45 +0300

    normalise data

--- src/prepare_datasets.py
+++ src/prepare_datasets.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from .load_datasets import load_datasets
 
+feature_list = ['High', 'Low', 'Open', 'Close']
+
 result_datasets_folder = './preprocessed'
 if not os.path.exists(result_datasets_folder):
     os.makedirs(result_datasets_folder)
@@ -21,11 +23,22 @@ def get_prepared_datasets():
 def build_prepared_dataset():
     train, test = load_datasets()
 
+    train = train[feature_list]
+    test = test[feature_list]
+
+    # Calc base deviation
+
+    train_mean = train.mean()
+    train_std = train.std()
+
+    train = (train - train_mean) / train_std
+    test = (test - train_mean) / train_std
+
     with open(processed_train_dataset_path, 'w') as f:
-        train.to_csv(f)
+        train.to_csv(f, index=False)
 
     with open(processed_test_dataset_path, 'w') as f:
-        test.to_csv(f)    
+        test.to_csv(f, index=False)    
 
 
 if __name__ == '__main__':

5f030e9932ab8583ab398ec132f88edf6520275d
leovs09 <leovs010@gmail.com>
2021-03-14 03:49:27 +0300

    fix local links

--- results/index.py
+++ results/index.py
@@ -12,11 +12,11 @@
 
 # ## Research steps:
 
-# 1. [Exploratory Data Analysis](/1_Exploratory_Data_Analysis.html) | [notebook version](./1_Exploratory_Data_Analysis.ipynb)
-# 2. [Feature Engeneering and Normalisation](/2_a_Feature_Engineering_and_Normalization) | [notebook version](./2_a_Feature_Engineering_and_Normalization.ipynb)
-# 3. [Window Generator](/2_b_Window_Generator.html) | [notebook version](./2_b_Window_Generator.ipynb)
-# 4. [Explore Training Metrics](/3_Explore_Training_Metrics.html) | [notebook version](./3_Explore_Training_Metrics.ipynb)
-# 5. [Test Model](/4_Test_model.html) | [notebook version](./4_Test_model.ipynb)
+# 1. [Exploratory Data Analysis](./1_Exploratory_Data_Analysis.html) | [notebook version](./1_Exploratory_Data_Analysis.ipynb)
+# 2. [Feature Engeneering and Normalisation](./2_a_Feature_Engineering_and_Normalization) | [notebook version](./2_a_Feature_Engineering_and_Normalization.ipynb)
+# 3. [Window Generator](./2_b_Window_Generator.html) | [notebook version](./2_b_Window_Generator.ipynb)
+# 4. [Explore Training Metrics](./3_Explore_Training_Metrics.html) | [notebook version](./3_Explore_Training_Metrics.ipynb)
+# 5. [Test Model](./4_Test_model.html) | [notebook version](./4_Test_model.ipynb)
 # 
 
 # In[ ]:
